f83c968425d51341736952ee273a980325a7fb05
Fatima Naz <naz781@gmail.com>
2025-12-10 18:34:24 -0500

    add gitignore

--- src/pages/SupabaseClient.js
+++ src/pages/SupabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://ypoubhaujgmpxrzhbwpt.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlwb3ViaGF1amdtcHhyemhid3B0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNTIyNjAsImV4cCI6MjA3OTcyODI2MH0.2M441mifIeF54HmMHryX3jAyhp2UxQ3b8n4K3htsJ2E";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
